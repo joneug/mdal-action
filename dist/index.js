ed5ba3d7f41281fc042cc79581d7d6b93a4a0cf5
Jonathan Neugebauer <jonathan.n@uni-muenster.de>
2020-07-14 11:10:25 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -55,14 +55,18 @@ module.exports =
 const core = __webpack_require__(310);
 const exec = __webpack_require__(230);
 
+const image = 'joneug/mdal';
+
 async function run() {
   try {
     let modelFile = core.getInput('model-file')
 
     if (process.platform.toLowerCase() === 'win32') {
-      await exec.exec('docker', ['run', '--rm', '-v', `${__dirname}:C:/project`, 'joneug/mdal', modelFile], {});
+      await exec.exec('docker', ['pull', image, '-q', '>', '$null'], {});
+      await exec.exec('docker', ['run', '--rm', '-v', `${__dirname}:C:/project`, image, modelFile], {});
     } else {
-      await exec.exec('docker', ['run', '--rm', '-v', `${__dirname}:/project`, 'joneug/mdal', modelFile], {});
+      await exec.exec('docker', ['pull', image, '-q', '>', '/dev/null'], {});
+      await exec.exec('docker', ['run', '--rm', '-v', `${__dirname}:/project`, image, modelFile], {});
     }
 
   } catch (error) {
--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
+const image = 'joneug/mdal';
+
 async function run() {
   try {
     let modelFile = core.getInput('model-file')
 
     if (process.platform.toLowerCase() === 'win32') {
-      await exec.exec('docker', ['run', '--rm', '-v', `${__dirname}:C:/project`, 'joneug/mdal', modelFile], {});
+      await exec.exec('docker', ['pull', image, '-q', '>', '$null'], {});
+      await exec.exec('docker', ['run', '--rm', '-v', `${__dirname}:C:/project`, image, modelFile], {});
     } else {
-      await exec.exec('docker', ['run', '--rm', '-v', `${__dirname}:/project`, 'joneug/mdal', modelFile], {});
+      await exec.exec('docker', ['pull', image, '-q', '>', '/dev/null'], {});
+      await exec.exec('docker', ['run', '--rm', '-v', `${__dirname}:/project`, image, modelFile], {});
     }
 
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
